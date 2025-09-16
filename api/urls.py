4dbdeeba10cef5a2a7ef5505dbae977a1e7bf0d8
Anjana Dhakad <anjanadhakad9584@gmail.com>
2025-09-17 00:03:17 +0530

    feat: Define URL routing for home and flowchart pages

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Jab koi http://.../api/generate-flowchart/ par request karega,
-    # toh views.generate_flowchart function chalega.
-    path('generate-flowchart/', views.generate_flowchart, name='generate_flowchart'),
+    # Yahan par humne name='home' add kiya hai
+    path('', views.index, name='home'), 
+    path('flowchart-to-code/', views.flowchart_to_code_view, name='flowchart-to-code'),
 ]
