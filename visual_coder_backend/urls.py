284ce3faf04d6ed2a23b83b03de8b299d0a45d1c
Anjana Dhakad <anjanadhakad9584@gmail.com>
2025-09-17 00:02:52 +0530

    feat: Include URL patterns for the api app

--- visual_coder_backend/urls.py
+++ visual_coder_backend/urls.py
@@ -1,38 +1,18 @@
-"""
-URL configuration for visual_coder_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-"""
-URL configuration for visual_coder_backend project.
-"""
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from api import views as api_views  # api/views.py se saare views import karein
 
 urlpatterns = [
-    # 1. Admin Panel ka URL
     path('admin/', admin.site.urls),
+    
+    # Yeh aapke API endpoints ke liye hai, jaise /api/generate-flowchart/
+    path('api/', include('api.urls')), 
+    
+    # Yeh NAYA path hai homepage ke liye. 
+    # Khali path ('') ka matlab hai root URL.
+    path('', api_views.index, name='homepage'), 
 
-    # 2. API ka URL
-    # Yeh line Django ko batati hai ki agar URL '/api/' se shuru hota hai,
-    # toh request ko 'api.urls' file ke paas bhej do.
-    path('api/', include('api.urls')),
+    path('code-to-flowchart/', api_views.code_to_flowchart_view, name='code_to_flowchart'),
 
-    # 3. React App ka URL (Catch-all)
-    # Yeh line sabse aakhir mein aati hai.
-    # Iska matlab hai ki agar URL upar kisi se match nahi hota,
-    # toh use React app (index.html) ke paas bhej do.
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    path('flowchart-to-code/', api_views.flowchart_to_code_view, name='flowchart_to_code'),
 ]
